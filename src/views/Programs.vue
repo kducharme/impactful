9e5895ea5b89822bda8b31f87e8afe75495b019c
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-06-14 13:55:14 -0500

    program cards working

--- src/views/Programs.vue
+++ src/views/Programs.vue
@@ -4,9 +4,48 @@
       <SubNavigation />
     </div>
     <div class="content">
-      <div class='card' v-for="program in programs" :key="program.id">
-        <h4>{{ program.name }}</h4>
-        <p>{{ program.description }}</p>
+      <div class='card' v-on:mouseover="showOverlay" v-on:mouseleave="hideOverlay"  v-for="program in programs" :key="program.id" >
+
+        <span class="card__hover hide">
+                <span class="select__container">
+                  <input type="checkbox" checked={this.state.checked} />
+                  <span class="select__checkbox" />
+                </span>
+                <button 
+                  class='card__button card__hover--button button__primary'
+                  to=''
+                  >
+                    View program
+                </button>
+              </span>
+        <div class="card__overview">
+          <h4>{{ program.name }}</h4>
+          <p>{{ program.description }}</p>
+  
+          <div class="card__data">
+            <div class="card__data--block">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path
+                          class="icon money"
+                          d="M11.8 10.9c-2.27-.59-3-1.2-3-2.15 0-1.09 1.01-1.85 2.7-1.85 1.78 0 2.44.85 2.5 2.1h2.21c-.07-1.72-1.12-3.3-3.21-3.81V3h-3v2.16c-1.94.42-3.5 1.68-3.5 3.61 0 2.31 1.91 3.46 4.7 4.13 2.5.6 3 1.48 3 2.41 0 .69-.49 1.79-2.7 1.79-2.06 0-2.87-.92-2.98-2.1h-2.2c.12 2.19 1.76 3.42 3.68 3.83V21h3v-2.15c1.95-.37 3.5-1.5 3.5-3.55 0-2.84-2.43-3.81-4.7-4.4z"
+                        />
+                        <path d="M0 0h24v24H0z" fill="none" />
+                      </svg>
+              <p>${{program.budget}} total budget</p>
+            </div>
+            <div class="card__data--block">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M0 0h24v24H0z" fill="none" />
+                        <path
+                          class="icon check"
+                          d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"
+                        />
+                      </svg>
+              <p>35 /45 tasks (90% complete)</p>
+            </div>
+            <div class="card__progress" />
+          </div>
+        </div>
   
       </div>
   
@@ -28,10 +67,20 @@
       programList: "programs"
     },
     methods: {
-      ...mapActions(["getPrograms"]),
+      ...mapActions([
+          "getPrograms"
+      ]),
       getProgramData: function() {
         this.getPrograms();
-      }
+      },
+      showOverlay: function(e) {
+        const overlay = e.currentTarget.children[0];
+        overlay.classList.remove("hide");
+      },
+      hideOverlay: function(e) {
+        const overlay = e.currentTarget.children[0];
+        overlay.classList.add("hide");
+      },
     },
     computed: {
       ...mapState(["programs"])
@@ -49,10 +98,9 @@
   @import "../styles/variables";
   @import "../styles/mixins";
   .content {
-    height: calc(10m0vh - 44px - 40px);
+    height: calc(100vh - 44px - 40px);
     @include display-flex(flex-start, flex-start, row);
     padding-top: 30px;
-
     .card {
       @include display-flex(flex-start, flex-start, column);
       position: relative;
@@ -173,7 +221,7 @@
         background: rgba(255, 255, 255, 0.418);
       }
       input[type="checkbox"]:checked+.select__checkbox {
-        background: $colorSuccess;
+        // background: $colorSuccess;
       }
       .card__hover--button {
         margin-top: 40px;
