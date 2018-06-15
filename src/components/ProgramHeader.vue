f62b4d4845fac3bac504bc9fbc0437fd2f6a0f89
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-06-15 09:50:09 -0500

    dynamic project count working

---
+++ src/components/ProgramHeader.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="header">
+        <div class="header__title">
+            <h2>All Projects <span>({{ projects.length }})</span></h2>
+            <div class="header__select">
+                <p class="header__select--selected">{{ selected }} selected</p>
+                <span><svg width="5px" height="5px" viewBox="0 0 5 5" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <g id="Discovery-(v2)" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <g id="Program-(Projects)" transform="translate(-518.000000, -157.000000)" fill="#E2E2E2">
+                        <circle id="Oval" cx="520.5" cy="159.5" r="2.5"></circle>
+                        </g>
+                        </g>
+                    </svg></span>
+                <a class="header__select--all">Select all</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  name: "header",
+  data() {
+    return {
+      totalProjects: 12,
+      selected: 0
+    };
+  },
+  computed: {
+    ...mapState(["projects"])
+  }
+  //   methods: {
+  //     ...mapActions(["getProjects"])
+  //   },
+  //   beforeMount() {
+  //     const id = window.location.href.split("programs/")[1];
+  //     this.getProjects(id);
+  //   }
+};
+</script>
+
+<style lang='scss'>
+@import "../styles/variables";
+@import "../styles/mixins";
+.header {
+  height: 80px;
+  @include display-flex(flex-start, center, row);
+  .header__title {
+    @include display-flex(flex-start, flex-start, column);
+    h2 {
+      margin-bottom: 4px;
+      font-size: 18px;
+      color: $colorFontDark;
+    }
+    .header__select {
+      @include display-flex(flex-start, flex-start, row);
+      font-size: 13px;
+      text-decoration: none;
+      p {
+        color: $colorFontMedium;
+      }
+      a {
+        color: $colorAccent;
+      }
+      span {
+        margin: 0 8px 0 8px;
+      }
+    }
+  }
+}
+</style>
--- src/views/Program.vue
+++ src/views/Program.vue
@@ -3,52 +3,56 @@
     <SubNavigation />
     <div class="content">
       <Groups />
-      <div class="content__projects">
-      <div class='card' v-for="project in projects" :key="project.id" :id="project.id">
-    
-        <div class="card__overview">
-          <h4>{{ project.name }}</h4>
-          <p>{{ project.description }}</p>
+      <div class="content__right">
+        <ProgramHeader />
+        <div class="content__projects">
+          <div class='card' v-for="project in projects" :key="project.id" :id="project.id">
   
-          <div class="card__data">
-            <div class="card__data--block">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <path
-                          class="icon money"
-                          d="M11.8 10.9c-2.27-.59-3-1.2-3-2.15 0-1.09 1.01-1.85 2.7-1.85 1.78 0 2.44.85 2.5 2.1h2.21c-.07-1.72-1.12-3.3-3.21-3.81V3h-3v2.16c-1.94.42-3.5 1.68-3.5 3.61 0 2.31 1.91 3.46 4.7 4.13 2.5.6 3 1.48 3 2.41 0 .69-.49 1.79-2.7 1.79-2.06 0-2.87-.92-2.98-2.1h-2.2c.12 2.19 1.76 3.42 3.68 3.83V21h3v-2.15c1.95-.37 3.5-1.5 3.5-3.55 0-2.84-2.43-3.81-4.7-4.4z"
-                        />
-                        <path d="M0 0h24v24H0z" fill="none" />
-                      </svg>
-              <p>${{project.budget}} total budget</p>
+            <div class="card__overview">
+              <h4>{{ project.name }}</h4>
+              <p>{{ project.description }}</p>
+              <div class="card__data">
+                <div class="card__data--block">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                <path
+                                  class="icon money"
+                                  d="M11.8 10.9c-2.27-.59-3-1.2-3-2.15 0-1.09 1.01-1.85 2.7-1.85 1.78 0 2.44.85 2.5 2.1h2.21c-.07-1.72-1.12-3.3-3.21-3.81V3h-3v2.16c-1.94.42-3.5 1.68-3.5 3.61 0 2.31 1.91 3.46 4.7 4.13 2.5.6 3 1.48 3 2.41 0 .69-.49 1.79-2.7 1.79-2.06 0-2.87-.92-2.98-2.1h-2.2c.12 2.19 1.76 3.42 3.68 3.83V21h3v-2.15c1.95-.37 3.5-1.5 3.5-3.55 0-2.84-2.43-3.81-4.7-4.4z"
+                                />
+                                <path d="M0 0h24v24H0z" fill="none" />
+                              </svg>
+                  <p>${{project.budget}} total budget</p>
+                </div>
+                <div class="card__data--block">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                <path d="M0 0h24v24H0z" fill="none" />
+                                <path
+                                  class="icon check"
+                                  d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"
+                                />
+                              </svg>
+                  <p>35/45 tasks (90%)</p>
+                </div>
+                <div class="card__progress" />
+              </div>
             </div>
-            <div class="card__data--block">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <path d="M0 0h24v24H0z" fill="none" />
-                        <path
-                          class="icon check"
-                          d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"
-                        />
-                      </svg>
-              <p>35/45 tasks (90%)</p>
-            </div>
-            <div class="card__progress" />
+  
           </div>
         </div>
-  
       </div>
-      </div>
-      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from "vuex";
 import SubNavigation from "../components/SubNavigation.vue";
+import ProgramHeader from "../components/ProgramHeader.vue";
 import Groups from "../components/Groups.vue";
 export default {
   name: "program",
   components: {
     SubNavigation,
+    ProgramHeader,
     Groups
   },
   methods: {
@@ -82,9 +86,7 @@ export default {
   .groups {
     background-color: white;
     border-right: 1px solid $grayBorder;
-    width: 25%;
-    min-width: 320px;
-    max-width: 400px;
+    width: 300px;
     height: 100%;
     .groups__header {
       @include display-flex(space-between, center, row);
@@ -107,13 +109,18 @@ export default {
       }
     }
   }
-  .content__projects {
-    @include display-flex(flex-start, flex-start, row);
-    flex-wrap: wrap;
-    overflow: scroll;
-    width: 73%;
-    padding: 2% 3.5% 4% 3.5%;
+  .content__right {
+    @include display-flex(flex-start, flex-start, column);
+    width: calc(100vw - 300px);
+    padding: 0 3% 4% 3%;
     height: calc(100vh - 44px - 40px);
+    .content__projects {
+      height: calc(100vh - 44px - 40px - 80px);
+
+      @include display-flex(flex-start, flex-start, row);
+      flex-wrap: wrap;
+      overflow: scroll;
+    }
   }
 }
 </style>
